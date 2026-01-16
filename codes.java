fdf7bd0962c7435eebf191f267b57fba2bbebb0b
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-16 10:48:59 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -20,6 +20,10 @@ class Solution {
 }
 
 
+
+
+
+
 2.Triplet Sum in Array
 
 
@@ -45,3 +49,110 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+
+
+
+3.Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        if (n < 4) return result;
+
+        Arrays.sort(nums); 
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;     
+                int right = n - 1;    
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(
+                                nums[i], nums[j], nums[left], nums[right]
+                        ));
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    }
+                    else if (sum < target) {
+                        left++;
+                    }
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
+
+4.Find triplets with zero sum
+
+Given an array arr[] of integers, determine whether it contains a triplet whose sum equals zero. Return true if such a triplet exists, otherwise, return false.
+
+    
+import java.util.*;
+
+class Solution {
+    public boolean findTriplets(int[] arr) {
+
+        int n = arr.length;
+        if (n < 3) return false;
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 2; i++) {
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    return true;
+                }
+                else if (sum > 0) {
+                    right--;
+                }
+                else {
+                    left++;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+    
+
+
+
+
+
+
