b408d5fbec4dc4f7cca6aa63297f1204cf9eba79
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-18 11:16:05 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -563,3 +563,46 @@ public class Main {
     }
 }
 
+
+
+BINARY SEARCH::
+
+1.Sorted Array Search
+
+Given an array, arr[] sorted in ascending order and an integer k. Return true if k is present in the array, otherwise, false.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 6], k = 6
+Output: true
+Exlpanation: Since, 6 is present in the array at index 4 (0-based indexing), output is true.
+Input: arr[] = [1, 2, 4, 5, 6], k = 3
+Output: false
+Exlpanation: Since, 3 is not present in the array, output is false.
+
+
+
+
+    class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {  
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == k) {
+                return true;
+            } 
+            else if (arr[mid] > k) {
+                right = mid - 1;
+            } 
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
