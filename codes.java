cf5aeec71e89a60db0576ddc830274759cb9f184
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-29 19:41:07 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -610,9 +610,7 @@ Exlpanation: Since, 3 is not present in the array, output is false.
 
 Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
 
-Note: In case of multiple occurrences of floor of x, return the index of the last occurrence.
 
-Examples
 
 Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 5
 Output: 1
@@ -639,6 +637,66 @@ Explanation: Largest number less than or equal to 5 is 2, whose index is 1
 
         return floorIndex;
     }
+}
+
+
+
+
+Problem statement
+You are given an infinite array consisting of only ones and zeroes, in sorted order. You have to find the index of the first occurrence of 1.
+
+Example:
+If the array is 0 0 0 0 1 1 1 1… then, the first occurrence of 1 will be at index 4 therefore the answer here is 4.
+Note:
+As the array size is infinite, the actual array won’t be given to you. Instead, you will be able to access the array elements by calling a method named ‘get’.
+
+get(i) : returns the value present at index I.
+
+Indexing is 0-based. 
+
+Instead of representing an infinite array in the input, we give the index of the first occurrence of 1 in the input itself. However, this input will be completely hidden from the user.
+
+It is guaranteed that the answer will fit in a 64-bit integer.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+0 <= ARR[i] <= 1
+
+Time limit: 1sec
+Sample Input 1:
+10
+Sample Output 1:
+10
+Sample Input 2:
+1
+Sample Output 2:
+1
+
+
+
+
+    public static long firstOne() {
+    long low = 0;
+    long high = 1;
+
+    // Step 1: Find range
+    while (get(high) == 0) {
+        low = high;
+        high = high * 2;
+    }
+
+    // Step 2: Binary Search
+    long ans = -1;
+    while (low <= high) {
+        long mid = low + (high - low) / 2;
+
+        if (get(mid) == 1) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
 }
 
 
