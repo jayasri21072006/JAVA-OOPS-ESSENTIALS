1042e16b2566b24b447ebd493059d8ce6da4e06e
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-29 19:30:55 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -606,3 +606,40 @@ Exlpanation: Since, 3 is not present in the array, output is false.
         return false;
     }
 }
+
+
+Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
+
+Note: In case of multiple occurrences of floor of x, return the index of the last occurrence.
+
+Examples
+
+Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 5
+Output: 1
+Explanation: Largest number less than or equal to 5 is 2, whose index is 1
+
+
+
+    class Solution {
+    public int findFloor(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        int floorIndex = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= x) {
+                floorIndex = mid;   // valid floor found
+                low = mid + 1;      // move right for last occurrence
+            } else {
+                high = mid - 1;     // move left
+            }
+        }
+
+        return floorIndex;
+    }
+}
+
+
+
