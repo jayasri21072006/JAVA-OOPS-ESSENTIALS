665a2d4749bd679387d1b6cb98b7b31ab5a95b1d
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-02-10 20:17:08 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -706,7 +706,7 @@ Sample Output 2:
 
 
 
-
+SUPER IMPORTATNT QUESTION 
 
 You are given an array with unique elements of stalls[], which denote the positions of stalls. You are also given an integer k which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible.
 
