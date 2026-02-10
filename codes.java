9452d654ff621fdf97569807b5d701679fd6ff6b
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-02-10 21:30:59 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -840,3 +840,88 @@ Examples:
 }
 
 
+3.Given an array arr[] where each element denotes the length of a board, and an integer k representing the number of painters available. Each painter takes 1 unit of time to paint 1 unit length of a board.
+
+Determine the minimum amount of time required to paint all the boards, under the constraint that each painter can paint only a contiguous sequence of boards (no skipping or splitting allowed).
+
+Examples:
+
+Input: arr[] = [5, 10, 30, 20, 15], k = 3
+Output: 35
+Explanation: The optimal allocation of boards among 3 painters is - 
+Painter 1 → [5, 10] → time = 15
+Painter 2 → [30] → time = 30
+Painter 3 → [20, 15] → time = 35
+Job will be done when all painters finish i.e. at time = max(15, 30, 35) = 35
+Input: arr[] = [10, 20, 30, 40], k = 2
+Output: 60
+Explanation: A valid optimal partition is - 
+Painter 1 → [10, 20, 30] → time = 60
+Painter 2 → [40] → time = 40
+Job will be complete at time = max(60, 40) = 60
+Input: arr[] = [100, 200, 300, 400], k = 1
+Output: 1000
+Explanation: There is only one painter, so the painter must paint all boards sequentially. The total time taken will be the sum of all board lengths, i.e., 100 + 200 + 300 + 400 = 1000.
+
+
+
+    import java.util.*;
+
+class Solution {
+
+    public int minTime(int[] arr, int k) {
+        
+        int n = arr.length;
+        
+        // If painters are more than boards,
+        // still valid (some painters may remain idle)
+        
+        int low = 0;
+        int high = 0;
+        
+        // Define search space
+        for (int length : arr) {
+            low = Math.max(low, length);  // largest single board
+            high += length;               // total sum of boards
+        }
+
+        int answer = high;
+
+        // Binary Search
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canPaint(arr, k, mid)) {
+                answer = mid;      // possible solution
+                high = mid - 1;    // try smaller maximum time
+            } else {
+                low = mid + 1;     // increase allowed time
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean canPaint(int[] arr, int k, int maxTime) {
+        
+        int painterCount = 1;
+        int currentTime = 0;
+
+        for (int length : arr) {
+
+            if (currentTime + length > maxTime) {
+                painterCount++;
+                currentTime = length;
+
+                if (painterCount > k)
+                    return false;
+            } else {
+                currentTime += length;
+            }
+        }
+
+        return true;
+    }
+}
+
+
