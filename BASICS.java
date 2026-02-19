1bb3c5a08bc4c1dd5fcbdf2951b6eee236a9c030
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-02-19 16:03:44 +0530

    Update BASICS.java

--- BASICS.java
+++ BASICS.java
@@ -193,3 +193,142 @@ LINKED LIST DELETION::
 2 -> 3 -> null
 3 -> null
 null
+
+
+
+
+  public class Linkedlist {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    // 1️⃣ Insert at Head
+    static Node insertAtHead(Node head, int val) {
+        return new Node(val, head);
+    }
+
+    // 2️⃣ Insert at Tail
+    static Node insertAtTail(Node head, int val) {
+
+        Node newnode = new Node(val);
+
+        if (head == null) {
+            return newnode;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newnode;
+
+        return head;
+    }
+
+    // 3️⃣ Insert at Position (0-based index)
+    static Node insertAtPosition(Node head, int position, int val) {
+
+        Node newnode = new Node(val);
+
+        if (position == 0) {
+            newnode.next = head;
+            return newnode;
+        }
+
+        Node temp = head;
+        int count = 0;
+
+        while (temp != null)  {
+            
+            count++;
+            if(count==position-1){
+                temp.next=new Node(val,temp.next);
+                break;
+                
+            }
+            temp=temp.next;
+        }
+
+        
+
+       
+
+        return head;
+    }
+
+    // 4️⃣ Insert After a Given Value
+    static Node insertAfterValue(Node head, int target, int val) {
+
+        Node temp = head;
+
+        while (temp != null) {
+
+            if (temp.data == target) {
+
+                Node newnode = new Node(val);
+                newnode.next = temp.next;
+                temp.next = newnode;
+
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        return head;
+    }
+
+    // 🔹 Print List
+    static void printList(Node head) {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        Node head = null;
+
+        head = insertAtHead(head, 3);
+        head = insertAtHead(head, 2);
+        head = insertAtHead(head, 1);
+
+        printList(head);
+
+        head = insertAtTail(head, 4);
+        printList(head);
+
+        head = insertAtPosition(head, 2, 99);
+        printList(head);
+
+        head = insertAfterValue(head, 3, 77);
+        printList(head);
+    }
+}
+
+
+
+1 -> 2 -> 3 -> null
+1 -> 2 -> 3 -> 4 -> null
+1 -> 2 -> 99 -> 3 -> 4 -> null
+1 -> 2 -> 99 -> 3 -> 77 -> 4 -> null
