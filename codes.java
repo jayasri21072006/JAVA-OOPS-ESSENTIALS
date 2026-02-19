8d1e86175138a484f4faddfc4fe50f9fb3555188
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-02-19 20:04:04 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -1263,3 +1263,48 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+
+
+
+Delete without head pointer
+
+
+    
+Difficulty: MediumAccuracy: 78.57%Submissions: 224K+Points: 4
+You are given a node del_node of a Singly Linked List where you have to delete this given node from the linked list but you are not given the head of the list.
+
+After deleting the given node:
+
+The number of nodes in the linked list should decrease by one.
+All the values before & after the del_node node should be in the same order.
+Note: It is guaranteed that there exists a node with a value equal to the del_node value and it will not be the last node of the linked list.
+
+Examples:
+
+Input: Linked List = 1 -> 2, del_node = 1
+Output: 2
+Explanation: After deleting 1 from the linked list, we have remaining nodes as 2.
+
+
+
+
+    class Solution {
+ 
+    void deleteNode(Node del_node) {
+        
+        // Copy data from next node
+        del_node.data = del_node.next.data;
+        
+        // Skip the next node
+        del_node.next = del_node.next.next;
+    }
+}
+
+
+
+
+
+
+
