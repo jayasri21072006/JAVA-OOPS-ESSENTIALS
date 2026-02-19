1c9af24634fdbe59ed1347623b1a00d794de93c8
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-02-19 19:38:53 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -1147,3 +1147,65 @@ class Solution {
     }
 }
 
+
+
+
+Intersection in Y Shaped Lists
+Difficulty: MediumAccuracy: 44.67%Submissions: 324K+Points: 4Average Time: 45m
+You are given the heads of two non-empty singly linked lists, head1 and head2, that intersect at a certain point. Return that Node where these two linked lists intersect.
+
+Note: It is guaranteed that the intersected node always exists.
+
+In the custom input you have to give input for CommonList which pointed at the end of both head1 and head2 to form a Y-shaped linked list.
+Examples:
+
+Input: head1: 10 -> 15 -> 30, head2: 3 -> 6 -> 9 -> 15 -> 30
+Output: 15
+Explanation: From the above image, it is clearly seen that the common part is 15 -> 30, whose starting point is 15.
+    
+Input: head1: 4 -> 1 -> 8 -> 5, head2: 5 -> 6 -> 1 -> 8 -> 5
+Output: 1
+Explanation: From the above image, it is clearly seen that the common part is 1 -> 8 -> 5, whose starting point is 1.
+
+
+
+
+    sol:
+Ultra Simple Mental Model
+
+Imagine two people walking different paths that merge.
+
+If both walk the same total distance,
+they must step onto the merge point at the same time.
+
+
+
+    class Solution {
+    public Node intersectPoint(Node head1, Node head2) {
+        
+        Node p1 = head1;
+        Node p2 = head2;
+        
+        // Traverse until both pointers meet
+        while (p1 != p2) {
+            
+            // If p1 reaches end, switch to head2
+            if (p1 == null) {
+                p1 = head2;
+            } else {
+                p1 = p1.next;
+            }
+            
+            // If p2 reaches end, switch to head1
+            if (p2 == null) {
+                p2 = head1;
+            } else {
+                p2 = p2.next;
+            }
+        }
+        
+        // Either intersection node OR null (if no intersection)
+        return p1;
+    }
+}
+
