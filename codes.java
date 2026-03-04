f42e3cc2c5337b90da436272343fe199886c1f06
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-03-04 18:38:37 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -2207,5 +2207,57 @@ class Solution {
 }
     
 
+Next Greater Element
+Difficulty: MediumAccuracy: 32.95%Submissions: 508K+Points: 4Average Time: 20m
+You are given an array arr[] of integers, the task is to find the next greater element for each element of the array in order of their appearance in the array. Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+If there does not exist next greater of current element, then next greater element for current element is -1.
 
+Examples
+
+Input: arr[] = [1, 3, 2, 4]
+Output: [3, 4, 4, -1]
+Explanation: The next larger element to 1 is 3, 3 is 4, 2 is 4 and for 4, since it doesn't exist, it is -1.
+Input: arr[] = [6, 8, 0, 1, 3]
+Output: [8, -1, 1, 3, -1]
+Explanation: The next larger element to 6 is 8, for 8 there is no larger elements hence it is -1, for 0 it is 1, for 1 it is 3 and then for 3 there is no larger element on right and hence -1.
+Input: arr[] = [1, 2, 3, 5]
+Output: [2, 3, 5, -1]
+Explanation: For a sorted array, the next element is 
+
+
+
+   import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        // Traverse from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            
+            // Remove smaller or equal elements
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            
+            // If stack is empty, no greater element
+            if (stack.isEmpty()) {
+                result.add(-1);
+            } else {
+                result.add(stack.peek());
+            }
+            
+            // Push current element
+            stack.push(arr[i]);
+        }
+        
+        // Reverse because we filled result from right to left
+        Collections.reverse(result);
+        
+        return result;
+    }
+}
 
