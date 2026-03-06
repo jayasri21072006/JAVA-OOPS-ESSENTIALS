b7e1e0e180a928571ca94d9ff8961775862176e4
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-03-06 11:41:49 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -2776,3 +2776,44 @@ class myQueue {
         return count;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+Queue Reversal
+Difficulty: EasyAccuracy: 77.98%Submissions: 161K+Points: 2
+Given a queue q containing integer elements, your task is to reverse the queue.
+
+Examples:
+
+Input: q[] = [5, 10, 15, 20, 25]
+Output: [25, 20, 15, 10, 5]
+Explanation: After reversing the given elements of the queue, the resultant queue will be 25 20 15 10 5.
+Input: q[] = [1, 2, 3, 4, 5]
+Output: [5, 4, 3, 2, 1]
+Explanation: After reversing the given elements of the queue, the resultant queue will be 5 4 3 2 1.
+
+
+
+    class Solution {
+    public void reverseQueue(Queue<Integer> q) {
+        Stack<Integer> stack=new Stack<>();
+        while(!q.isEmpty()){
+            stack.push(q.remove());
+        }
+        while(!stack.isEmpty()){
+            q.add(stack.pop());
+        }
+        return;
+        
+    }
+}
