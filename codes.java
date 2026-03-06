11e760469e3c57716ee4107e6cc91d2b86934aa3
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-03-06 10:44:49 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -2538,3 +2538,129 @@ class Solution {
         return result.toString();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Queue Using Array
+Difficulty: BasicAccuracy: 47.24%Submissions: 278K+Points: 1Average Time: 15m
+Implement a Queue using an Array, where the size of the array, n is given.
+The Queue must support the following operations:
+
+(i) enqueue(x): Insert an element x at the rear of the queue.
+(ii) dequeue(): Remove the element from the front of the queue.
+(iii) getFront(): Return front element if not empty, else -1.
+(iv) getRear(): Return rear element if not empty, else -1.
+(v) isEmpty(): Return true if the queue is empty else return false.
+(vi) isFull(): Return true if the queue is full else return false.
+
+There will be a sequence of queries queries[][]. The queries are represented in numeric form:
+
+1 x : Call enqueue(x)
+2: Call dequeue()
+3: Call getFront()
+4: Call getRear()
+5: Call isEmpty()
+6: Call isFull()
+
+
+
+
+
+    class myQueue {
+
+    int[] arr;
+    int front;
+    int rear;
+    int size;
+    int count;
+
+    // Constructor
+    public myQueue(int n) {
+        size = n;
+        arr = new int[n];
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+
+    // Check if queue is empty
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    // Check if queue is full
+    public boolean isFull() {
+        return count == size;
+    }
+
+    // Insert element at rear
+    public void enqueue(int x) {
+
+        if (isFull()) {
+            return;
+        }
+
+        rear = (rear + 1) % size;
+        arr[rear] = x;
+        count++;
+    }
+
+    // Remove element from front
+    public void dequeue() {
+
+        if (isEmpty()) {
+            return;
+        }
+
+        front = (front + 1) % size;
+        count--;
+    }
+
+    // Get front element
+    public int getFront() {
+
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return arr[front];
+    }
+
+    // Get rear element
+    public int getRear() {
+
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return arr[rear];
+    }
+}
