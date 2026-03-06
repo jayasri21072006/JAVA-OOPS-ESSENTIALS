c87d7ed3dd8653c4856e02fb5d3aabb89c170c0e
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-03-06 11:59:06 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -2817,3 +2817,61 @@ Explanation: After reversing the given elements of the queue, the resultant queu
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+Gas Station
+Difficulty: MediumAccuracy: 34.79%Submissions: 221K+Points: 4Average Time: 20m
+There are n gas stations along a circular tour. You are given two integer arrays gas[] and cost[], where gas[i] is the amount of gas available at station i and cost[i] is the gas needed to travel from station i to station (i+1). You have a car with an unlimited gas tank and start with an empty tank at some station. Your task is to return the index of the starting station if it is possible to travel once around the circular route in a clockwise direction without running out of gas at any station; otherwise, return -1.
+
+Note: If a solution exists, it is guaranteed to be unique.
+
+Examples:
+
+Input: gas[] = [4, 5, 7, 4], cost[]= [6, 6, 3, 5]
+Output: 2
+Explanation: Start at gas station at index 2 and fill up with 7 units of gas. Your tank = 0 + 7 = 7
+Travel to station 3. Available gas = (7 – 3 + 4) = 8.
+Travel to station 0. Available gas = (8 – 5 + 4) = 7.
+Travel to station 1. Available gas = (7 – 6 + 5) = 6.
+Return to station 2. Available gas = (6 – 6) = 0.
+
+
+
+
+
+    class Solution {
+    public int startStation(int[] gas, int[] cost) {
+        
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+        
+        for(int i = 0; i < gas.length; i++){
+            
+            int diff = gas[i] - cost[i];
+            
+            total += diff;
+            tank += diff;
+            
+            if(tank < 0){
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        
+        if(total < 0){
+            return -1;
+        }
+        
+        return start;
+    }
+}
