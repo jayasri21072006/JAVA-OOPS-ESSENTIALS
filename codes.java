3169109beed0d52433eb5b934f777c725ac4d369
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-03-06 11:29:32 +0530

    Update codes.java

--- codes.java
+++ codes.java
@@ -2664,3 +2664,115 @@ There will be a sequence of queries queries[][]. The queries are represented in 
         return arr[rear];
     }
 }
+
+
+
+
+
+Queue using Linked List
+Difficulty: BasicAccuracy: 45.6%Submissions: 176K+Points: 1Average Time: 20m
+Implement a Queue using a Linked List, this queue has no fixed capacity and can grow dynamically until memory is available.
+The Queue must support the following operations:
+
+(i) enqueue(x): Insert an element x at the rear of the queue.
+(ii) dequeue(): Remove the element from the front of the queue.
+(iii) getFront(): Return front element if not empty, else -1.
+(iv) isEmpty(): Return true if the queue is empty else return false.
+(v) size(): Return the number of elements currently in the queue.
+
+There will be a sequence of queries queries[][]. The queries are represented in numeric form:
+
+1 x : Call enqueue(x)
+2: Call dequeue()
+3: Call getFront()
+4: Call isEmpty()
+5: Call size()
+You just have to implement the functions enqueue, dequeue, getFront,  isEmpty and size. The driver code will handle the input and output.
+
+Examples:
+
+Input: q = 7, queries[][] = [[1, 5], [1, 3], [1, 4], [3], [2], [5], [4]]
+Output: [5, 2, false]
+Explanation: Queries on queue are as follows:
+enqueue(5): Insert 5 at the rear of the queue.
+enqueue(3): Insert 3 at the rear of the queue.
+enqueue(4): Insert 4 at the rear of the queue.
+getFront(): Return the front element i.e 5.
+dequeue(): Remove the front element 5 from the queue.
+size(): Queue now has 2 elements.
+isEmpty(): Queue is not empty return false.
+
+
+
+
+    // Node class
+class Node {
+    int data;
+    Node next;
+
+    Node(int new_data) {
+        data = new_data;
+        next = null;
+    }
+}
+
+// Queue class
+class myQueue {
+
+    Node front;
+    Node rear;
+    int count;
+
+    public myQueue() {
+        front = null;
+        rear = null;
+        count = 0;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public void enqueue(int x) {
+
+        Node newNode = new Node(x);
+
+        if (isEmpty()) {
+            front = newNode;
+            rear = newNode;
+        } 
+        else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+
+        count++;
+    }
+
+    public void dequeue() {
+
+        if (isEmpty()) {
+            return;
+        }
+
+        front = front.next;
+        count--;
+
+        if (front == null) {
+            rear = null;
+        }
+    }
+
+    public int getFront() {
+
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return front.data;
+    }
+
+    public int size() {
+        return count;
+    }
+}
