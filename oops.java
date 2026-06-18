ed59cca0fae149cef0c9190bfdf80fab66f84d12
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-06-18 07:29:33 -0500

    Create oops.java

---
+++ oops.java
@@ -0,0 +1,16 @@
+public class main{
+    public static void main(String args[]){
+        add();
+        System.out.println("I Love You");
+    }
+    public static void add(){
+        int a=12;
+        int b=14;
+        System.out.println(a+b);
+    }
+}
+
+
+
+26
+I Love You
